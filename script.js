c4df11dd73d9634a6b9c46bb6e82ce14327a6755
aramk007 <aramk007@gmail.com>
2023-11-09 15:38:38 -0700

    new update

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("hello")
+let name = "Adam"
+console.log(name)
+console.log(typeof name);
+var number = "5"
+console.log(number)
+console.log(typeof number)
+var boolean = true
+console.log(boolean)
+console.log(typeof boolean)
+name = "adam"
+name ="yoooo"
+console.log(name)
+// let name ="Michael"
+// console.log(name)
+let secondNumber=5
+if (number===secondNumber) {
+console.log ("sameType AndValue")
+}
+else if (number==secondNumber) {
+console.log ("differentType SameValue")
+}
+else {
+    console.log ("DifferentType AndValue")
+}
